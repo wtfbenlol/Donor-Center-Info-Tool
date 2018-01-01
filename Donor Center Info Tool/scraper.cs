d505870069cb22c537ef5706cfaa4c0da72441c0
wtfbenlol <wtfbenlol@outlook.com>
2017-12-31 23:54:29 -0500

    Update
    Added more error handling and finished about window

--- Donor Center Info Tool/scraper.cs
+++ Donor Center Info Tool/scraper.cs
@@ -1,36 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using HtmlAgilityPack;
 using System.IO;
-
+using System.Net;
+using HtmlAgilityPack;
 
 namespace Donor_Center_Info_Tool
 {
-    class Scraper
+    internal class Scraper
     {
-        public string Scrape(string url)
+        public void Scrape(string url)
         {
             var document = new HtmlDocument();
-            string x = "";
+
             using (var client = new WebClient())
             {
-                
                 var page = client.DownloadString(url);
                 document.LoadHtml(page);
             }
 
             foreach (var node in document.DocumentNode.SelectNodes("//pre"))
             {
-                string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
                 File.Create(desktop + @"\zebra.txt").Close();
                 File.WriteAllText(desktop + @"\zebra.txt", node.InnerText);
                 //Console.WriteLine(node);
             }
-            return x;
         }
     }
 }
